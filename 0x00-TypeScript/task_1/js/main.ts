bb8ae613fcc1b0293048c81a45e13002f1d93fc9
alaedine07 <alaedineboufaden@gmail.com>
2022-05-31 17:49:22 +0100

    print teachers

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -6,3 +6,18 @@ interface Teacher {
     location: string;
     [key: string]: any;
 }
+
+interface Directors extends Teacher {
+    numberOfReporters: number;
+}
+
+function printTeacher (firstName: string, lastName: string): void {
+    const firstLetter = firstName.charAt(0).toUpperCase();
+    console.log(`${firstLetter}. ${lastName}`);
+}
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): void;
+}
+
+printTeacher('john', 'Doe');
