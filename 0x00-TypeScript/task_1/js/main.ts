ca83baedab36e06a8c9bf9b8dcab77a41ed9c3e5
alaedine07 <alaedineboufaden@gmail.com>
2022-05-31 18:15:14 +0100

    writing a class

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -21,3 +21,36 @@ interface printTeacherFunction {
 }
 
 printTeacher('john', 'Doe');
+
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+interface classConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+  }
+
+  class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+      return 'Currently working';
+    }
+
+    displayName(): string {
+      return this.firstName;
+    }
+  }
+
+  const student: StudentClass = new StudentClass('lionel', 'messi')
+  console.log(student.workOnHomework());
+  console.log(student.displayName());
