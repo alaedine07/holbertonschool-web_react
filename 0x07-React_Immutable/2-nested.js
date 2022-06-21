a1022ab64525753ea66dd284925b43ad14ed616c
alaedine07 <alaedineboufaden@gmail.com>
2022-06-21 17:56:41 +0100

    nested elements

---
+++ 0x07-React_Immutable/2-nested.js
@@ -0,0 +1,6 @@
+import { fromJS, getIn } from 'immutable';
+
+export default function accessImmutableObject (object, array){
+  const map = fromJS(object);
+  return getIn(map, array, undefined);
+}
