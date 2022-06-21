52ba54d001581c1b800b0279ff4551c4663e222d
alaedine07 <alaedineboufaden@gmail.com>
2022-06-21 17:59:34 +0100

    fix semistandart

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,6 +1,6 @@
 import { fromJS, getIn } from 'immutable';
 
-export default function accessImmutableObject (object, array){
+export default function accessImmutableObject(object, array) {
   const map = fromJS(object);
   return getIn(map, array, undefined);
 }
