f6f4ba3cd0e04e61fee415f4f596cfb8003f9864
alaedine07 <alaedineboufaden@gmail.com>
2022-07-01 18:07:38 +0100

    first inline styling

---
+++ 0x05-React_inline_styling/task_0/dashboard/CourseList/CourseListRow.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+export default function CourseListRow(props) {
+  const rowStyle = {backgroundColor: '#f5f5f5ab'};
+  const headerStyle = {backgroundColor: '#deb5b545'};
+  const isHeader = props.isHeader;
+  const textFirstCell = props.textFirstCell;
+  const textSecondCell = props.textSecondCell;
+  if (isHeader) {
+      if (!textSecondCell) {
+          return (
+            <tr style={rowStyle}>
+              <th colSpan={2}>{textFirstCell}</th>
+            </tr>
+          );
+      }
+      if (textSecondCell != null) {
+          return (
+            <tr>
+              <th style={headerStyle}>{textFirstCell}</th>
+              <th style={headerStyle}>{textSecondCell}</th>
+            </tr>
+          );
+      }
+  }
+  else {
+      <td>{textFirstCell} {textSecondCell}</td>
+  }
+}
+CourseListRow.prototype = {
+  textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
